ea72062844ef8cb39c8b422a6c6f72b51cec4f19
josueCalcina <51217381+josueCalcina@users.noreply.github.com>
2022-01-10 09:15:01 -0300

    vistas Terminar Actividad

---
+++ resources/views/finalizar.blade.php
@@ -0,0 +1,36 @@
+@extends('adminlte::page')
+@section('template_title')
+    Terminar Actividad
+@endsection
+
+@section('content')
+    <section class="content container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+
+                @includeif('partials.errors')
+
+                <form method="POST" action=""  role="form" enctype="multipart/form-data">
+                    @csrf
+
+                    @include('formFinalizar')
+
+                </form>
+
+                {{-- <div class="card card-default">
+                    <div class="card-header">
+                        <span class="card-title">Create Actividade</span>
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('actividades.store') }}"  role="form" enctype="multipart/form-data">
+                            @csrf
+
+                            @include('actividade.form')
+
+                        </form>
+                    </div>
+                </div> --}}
+            </div>
+        </div>
+    </section>
+@endsection
