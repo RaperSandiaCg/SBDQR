e4fb9aab0840359de5e1841a93f42eecf952f46b
josueCalcina <51217381+josueCalcina@users.noreply.github.com>
2022-01-17 16:33:38 -0300

    carbon en app

--- resources/views/actividade/edit.blade.php
+++ resources/views/actividade/edit.blade.php
@@ -1,4 +1,4 @@
-{{-- @extends('layouts.app')
+{{-- @extends('layouts.app') --}}
 @extends('adminlte::page')
 @section('template_title')
     Update Actividade
@@ -25,43 +25,6 @@
                         </form>
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
-@endsection --}}
-
-@extends('adminlte::page')
-@section('template_title')
-    Terminar Actividad
-@endsection
-
-@section('content')
-    <section class="content container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-
-                @includeif('partials.errors')
-
-                <form method="POST" action=""  role="form" enctype="multipart/form-data">
-                    @csrf
-
-                    @include('actividade.formTerminar')
-
-                </form>
-
-                {{-- <div class="card card-default">
-                    <div class="card-header">
-                        <span class="card-title">Create Actividade</span>
-                    </div>
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('actividades.store') }}"  role="form" enctype="multipart/form-data">
-                            @csrf
-
-                            @include('actividade.form')
-
-                        </form>
-                    </div>
-                </div> --}}
             </div>
         </div>
     </section>
--- resources/views/formFinalizar.blade.php
+++ resources/views/formFinalizar.blade.php
@@ -18,28 +18,17 @@
                 <div class="input-group-prepend">
                 <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
                 </div>
-                <input type="text" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" value="10/01/2022" data-mask="" im-insert="false">
+                <input type="text" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" placeholder="dd/mm/yyyy" data-mask="" im-insert="false">
             </div>
             <!-- /.input group -->
           </div>
 
-        <!-- Fecha de termino -->
-          <div class="form-group">
-            <label><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Fecha de termino:</font></font></label>
-
-            <div class="input-group">
-                <div class="input-group-prepend">
-                <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
-                </div>
-                <input type="text" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" value="10/01/2022" data-mask="" im-insert="false">
-            </div>
-            <!-- /.input group -->
-          </div>
+          <!-- Fecha de termino -->
 
           <!-- Actividad -->
           <div class="form-group">
             <label for="exampleInputPassword1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Actividad:</font></font></label>
-            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Mantención">
+            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Bomba N2">
           </div>
 
           <!-- Encargado Rut-->
@@ -53,14 +42,13 @@
 
           <div class="form-group">
             <label for="exampleInputPassword1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Rut Encargado:</font></font></label>
-            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="17654898-9">
+            <input type="text" class="form-control" id="exampleInputPassword1" placeholder="12345678-9">
           </div>
 
           {{-- Puntos de Aislamiento --}}
           <div class="form-group">
             <label><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Puntos de Aislamiento</font></font></label>
-            <select class="form-control select2 select2-hidden-accessible" style="width: 100%;" data-select2-id="1" tabindex="-1" aria-hidden="true">      
-              <option hidden><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">3 Punto de Aislamiento</font></font></option>            
+            <select class="form-control select2 select2-hidden-accessible" style="width: 100%;" data-select2-id="1" tabindex="-1" aria-hidden="true">                  
               <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">1 Punto de Aislamiento</font></font></option>
               <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">2 Puntos de Aislamiento</font></font></option>
               <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">3 Puntos de Aislamiento</font></font></option>
@@ -74,7 +62,7 @@
           <div class="form-group">
             <label><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Rut empresa encargada</font></font></label>
             <select class="form-control select2 select2-hidden-accessible" style="width: 100%;" data-select2-id="1" tabindex="-1" aria-hidden="true">                  
-              <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">89456156-9</font></font></option>
+              <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">12345678-9</font></font></option>
              </select>{{--<span class="select2 select2-container select2-container--default" dir="ltr" data-select2-id="2" style="width: 100%;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-disabled="false" aria-labelledby="select2-msie-container"><span class="select2-selection__rendered" id="select2-msie-container" role="textbox" aria-readonly="true" title="Alabama"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Alabama</font></font></span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span> --}}
           </div>
 
@@ -87,25 +75,11 @@
           </div>
 
         </div>
-
-        <div class="form-group">
-            <label for="exampleInputFile"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Entrada de archivo</font></font></label>
-            <div class="input-group">
-              <div class="custom-file">
-                <input type="file" class="custom-file-input" id="exampleInputFile">
-                <label class="custom-file-label" for="exampleInputFile"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Elija el archivo</font></font></label>
-              </div>
-              <div class="input-group-append">
-                <span class="input-group-text" id=""><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Subir</font></font></span>
-              </div>
-            </div>
-        </div>
-
         <!-- /.card-body -->
 
         <div class="card-footer">
-            <a href="vista1" class="btn btn-primary btn-sm"  data-placement="left">
-              {{ __('Terminar actividad') }}
+            <a href="{{ route('actividades.index') }}" class="btn btn-primary btn-sm"  data-placement="left">
+              {{ __('Agregar Registro') }}
             </a>
         </div>
 
